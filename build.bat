a9964e8365a7cc8471ea5a9f550f95d1082a6700
ten4dinosaur <113720384+ten4dinosaur@users.noreply.github.com>
2023-06-14 20:50:14 +0200

    Update build.bat

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ set "clang_executable=%clang_executable:\=/%"
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvarsall.bat" %1
 cd boringssl
 mkdir build
-cmake "-DCMAKE_TOOLCHAIN_FILE=%toolchain%" -DBUILD_SHARED_LIBS=1 -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_LINKER=link.exe "-DCMAKE_ASM_NASM_COMPILER=C:/Program Files/NASM/nasm.exe" "-DCMAKE_CXX_COMPILER=%clang_executable%" "-DCMAKE_C_COMPILER=%clang_executable%" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DPERL_EXECUTABLE=C:/Strawberry/perl/bin/perl.exe" -B build
+cmake "-DCMAKE_TOOLCHAIN_FILE=%toolchain%" -DCMAKE_BUILD_TYPE=Release -GNinja -DCMAKE_LINKER=link.exe "-DCMAKE_ASM_NASM_COMPILER=C:/Program Files/NASM/nasm.exe" "-DCMAKE_CXX_COMPILER=%clang_executable%" "-DCMAKE_C_COMPILER=%clang_executable%" "-DCMAKE_MAKE_PROGRAM=ninja.exe" "-DPERL_EXECUTABLE=C:/Strawberry/perl/bin/perl.exe" -B build
 ninja -C build
 cd build
-7z a build.zip crypto\crypto.dll crypto\crypto.dll.manifest crypto\crypto.lib ssl\ssl.dll ssl\ssl.dll.manifest ssl\ssl.lib
+7z a build.zip ./*
